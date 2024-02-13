3955af36490e12ab69a6319886507b2870a3af12
Bharati Singh <88208046+SinghBharati@users.noreply.github.com>
2024-02-14 01:03:21 +0530

    Optimized theme button

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,6 +7,7 @@ function AboutMe() {
     return (
         <div className={`${theme ? "bg-gray-900 text-gray-200" : "bg-gray-200 text-gray-900"} p-5 `}>
             <h1 className="text-3xl font-bold py-3">About Me</h1>
+            <h3 className="text-3xl font-bold py-3">This is about me Section.</h3>
             <SkillSection/>
             <ProjectSection/>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -12,32 +12,18 @@ const Header = () => {
         setIsDarkMode(value)
     }
 
-
     return (
         <div className={isDarkMode ? "bg-gray-900 text-gray-200" : "bg-gray-200 text-gray-900"}>
             <ul className="p-4 flex justify-end">
                 <li className={"p-3 rounded-2xl hover:text-gray-500 hover:cursor-pointer"}><Link to="/">Home</Link></li>
-                <li className="p-3 rounded-2xl hover:text-gray-500 hover:cursor-pointer"><Link to="/about">About Me</Link></li>
-                <li className="p-3 rounded-2xl hover:text-gray-500 hover:cursor-pointer"><Link to="/contact">Contact Me</Link></li>
-                {/*<li className="p-3 rounded-2xl  text-gray-200 hover:text-gray-500 hover:cursor-pointer"*/}
-                {/*    onClick={handleTheme}*/}
-                {/*>{isDarkMode ? "Light" : "Dark"}*/}
-                {/*</li>*/}
+                <li className="p-3 rounded-2xl hover:text-gray-500 hover:cursor-pointer"><Link to="/about">About</Link></li>
+                <li className="p-3 rounded-2xl hover:text-gray-500 hover:cursor-pointer"><Link to="/contact">Contact</Link></li>
                 <li className="p-3 rounded-2xl  text-gray-200 hover:text-gray-500 hover:cursor-pointer">
-                    {isDarkMode ? (
-                        <button
-                            className="text-xl p-1 -my-1 rounded bg-gray-200 text-gray-900 font-bold"
-                            onClick={() => changeTheme(false)}
-                        >Light Mode
-                        </button>
-                    ):(
-                        <button
-                            className="text-xl p-1 -my-1 rounded bg-gray-900 text-gray-200 font-bold"
-                            onClick={() => changeTheme(true)}
-                        >Dark Mode
-                        </button>
-                    )
-                    }
+                    <button
+                        className={`${isDarkMode ? "bg-gray-200 text-gray-900" : "bg-gray-900 text-gray-200"}  text-xl p-1 -my-1 rounded font-bold`}
+                        onClick={() => changeTheme(!theme)}
+                    >{isDarkMode ? ("Light Mode") : ("Dark Mode")}
+                    </button>
                 </li>
             </ul>
         </div>
